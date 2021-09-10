9ae0a4fb8cb235f03e1fc6b0c3bb41b29d30f414
德芙 <13298419015@163.com>
2021-09-10 13:53:10 +0800

    build

---
+++ precache-manifest.a03a08bef5b6fc591a21858ecdac6eaf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59628d641477fa094ff4cb78bef123ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ebae92e78e6e9f0cad45",
+    "url": "/static/css/main.b4774b0a.chunk.css"
+  },
+  {
+    "revision": "e848915b46b1f21f71c4",
+    "url": "/static/js/2.da82b848.chunk.js"
+  },
+  {
+    "revision": "ebae92e78e6e9f0cad45",
+    "url": "/static/js/main.e5a8be40.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
